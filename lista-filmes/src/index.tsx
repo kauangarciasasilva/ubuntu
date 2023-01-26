5ef0dac3add8536e1ca09fd92c5f51ba2b8554b8
Nilton Diniz <niltondiniz@niltondiniz.com>
2023-01-26 03:30:08 +0000

    projeto finalizado

--- lista-filmes/src/index.tsx
+++ lista-filmes/src/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './global.css';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Details from './pages/details';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/details/:id' element={<Details />}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
